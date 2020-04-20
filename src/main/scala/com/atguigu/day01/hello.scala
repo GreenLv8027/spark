5f9cefc60d8cc1fe7678e754c10b4e145eceb358
greenteawang <xlcau2016@126.com>
2020-04-20 23:36:21 +0800

    第一次更新

---
+++ src/main/scala/com/atguigu/day01/hello.scala
@@ -0,0 +1,15 @@
+package com.atguigu.day01
+
+object hello {
+  def main(args: Array[String]): Unit = {
+
+    val a: Short=128
+    println(a)
+    val b: Byte=a.toByte
+    println(b)
+
+
+  }
+
+
+}
