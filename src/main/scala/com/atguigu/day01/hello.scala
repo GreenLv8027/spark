99bc9c80b75558960f922d7f7b9ff6bfde4f087c
greenteawang <xlcau2016@126.com>
2020-04-20 23:48:45 +0800

    第三次更新

--- src/main/scala/com/atguigu/day01/hello.scala
+++ src/main/scala/com/atguigu/day01/hello.scala
@@ -3,7 +3,7 @@ package com.atguigu.day01
 object hello {
   def main(args: Array[String]): Unit = {
 
-    val a: Short=122
+    val a: Short=125
     println(a)
     val b: Byte=a.toByte
     println(b)
